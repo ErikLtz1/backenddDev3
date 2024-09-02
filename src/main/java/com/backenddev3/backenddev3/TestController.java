3ff34fc0f5cf79ea3bd23011ca1cb777549841b6
David Hankin <d_hankin@outlook.com>
2024-09-02 12:59:09 +0200

    test deploy change

--- src/main/java/com/backenddev3/backenddev3/TestController.java
+++ src/main/java/com/backenddev3/backenddev3/TestController.java
@@ -9,10 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     
-
     @GetMapping("")
     public String getMethodName() {
-        return "Server Working";
+        return "Server Working - test push";
     }
     
 }
