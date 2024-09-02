9930c54b32b3985a7e70fb31da18bd3d03adb9a7
ErikLtz1 <eriklilliecreutz@gmail.com>
2024-09-02 12:45:52 +0200

    docker

---
+++ src/main/java/com/backenddev3/backenddev3/TestController.java
@@ -0,0 +1,18 @@
+package com.backenddev3.backenddev3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+    
+
+    @GetMapping("")
+    public String getMethodName() {
+        return "Server Working";
+    }
+    
+}
