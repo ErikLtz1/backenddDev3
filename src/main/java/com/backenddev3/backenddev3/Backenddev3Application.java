3f00ee09d380d6f71d6ad761e4b9a80ca6d09de4
ErikLtz1 <eriklilliecreutz@gmail.com>
2024-09-02 12:39:43 +0200

    hej

---
+++ src/main/java/com/backenddev3/backenddev3/Backenddev3Application.java
@@ -0,0 +1,13 @@
+package com.backenddev3.backenddev3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backenddev3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backenddev3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/backenddev3/backenddev3/Backenddev3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backenddev3.backenddev3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backenddev3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
