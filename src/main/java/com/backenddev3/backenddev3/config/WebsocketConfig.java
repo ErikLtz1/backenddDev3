85c488f1ef66987179a846eb02cf174d91cbd9d9
David Hankin <d_hankin@outlook.com>
2024-09-03 15:37:12 +0200

    first websocket connection

---
+++ src/main/java/com/backenddev3/backenddev3/config/WebsocketConfig.java
@@ -0,0 +1,25 @@
+package com.backenddev3.backenddev3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+    
+
+    @Override
+    public void configureMessageBroker(@SuppressWarnings("null") MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/destroy");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(@SuppressWarnings("null") StompEndpointRegistry registry) {
+        registry.addEndpoint("/player").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+}
